5846a2034e2ba3d5ff2f3b97916b1cb03de93a90
Manuel Rodriguez <supermanue@gmail.com>
2023-04-07 21:38:50 +0200

    test CD

--- app/src/main/kotlin/org/supermanue/App.kt
+++ app/src/main/kotlin/org/supermanue/App.kt
@@ -6,15 +6,11 @@ package org.supermanue
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 
-// 1
 class App : RequestHandler<HandlerInput, HandlerOutput> {
-    // 2
     val translator : PirateTranslator =   DefaultPirateTranslator()
 
-    // 3
     override fun handleRequest(input: HandlerInput?, context: Context?): HandlerOutput {
         input?.let {
-            // 4
             return HandlerOutput(it.message, translator.translate(it.message))
         }
         return HandlerOutput("", "");
